80c57defb56cd1b1f19a1217a689241ff60bbda3
Joachim Åsen Kvamme <150397418+JoachimKvamme@users.noreply.github.com>
2025-04-08 13:00:46 +0200

    max_sequence ferdig

---
+++ maximum_subarray_sum.py
@@ -0,0 +1,14 @@
+def max_sequence(array):
+    sum = 0
+    intermediarySum = 0
+    for number in array:
+        intermediarySum += number
+        if intermediarySum < 0:
+            intermediarySum = 0
+        if intermediarySum >= sum:
+            sum = intermediarySum
+    return sum
+
+print(max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+
+    
