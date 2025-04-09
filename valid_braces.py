6e547b60812997c0c51dd9b29cf11cf0931afea2
Joachim Åsen Kvamme <150397418+JoachimKvamme@users.noreply.github.com>
2025-04-09 09:44:11 +0200

    valid braces ferdig

--- valid_braces.py
+++ valid_braces.py
@@ -1,68 +1,45 @@
 
 def valid_braces(string):
-    if not check_if_correct_number_of_braces(string):
-        return False
-    if not check_distance_between_braces(string):
-        return False
-    return True
-
-def check_if_correct_number_of_braces(string):
-    if string.count("{") != string.count("}"):
-        return False
-    if string.count("(") != string.count(")"):
-        return False
-    if string.count("[") != string.count("]"):
-        return False
-    return True
-
-def check_distance_between_braces(string):
-    for i, brace in enumerate(string):
-        if brace == "{":
-            if string[i:].find("}") == -1:
-                return False
-            distance = string.find("}", i) - i
-            print(distance)
-            if distance % 2 == 0:
-                return False
-        if brace == "[":
-            if string.find("]", i) == -1:
-                return False
-            distance = string.find("]", i) - i
-            print(distance)
-            if distance % 2 == 0:
-                return False
-        if brace == "(":
-            if string.find(")", i) == -1:
-                return False
-            distance = string.find(")", i) - i
-            print(distance)
-            if distance % 2 == 0:
-                return False
-    return True
-
-def check_order_of_braces(string):
-    openingOrder = []
-    closingOrder = []
+    openingBraces = []
+    check_number_of_braces(string)
     for brace in string:
-        if brace == "{":
-            openingOrder.append(1)
-        if brace == "[":
-            openingOrder.append(2)
-        if brace == "(":
-            openingOrder.append(3)
+        if brace == "{" or brace == "[" or brace == "(":
+            openingBraces.append(brace)
         if brace == "}":
-            closingOrder.append(1)
+            if len(openingBraces) == 0:
+                return False
+            if openingBraces[-1] != "{":
+                return False
+            openingBraces.pop()
         if brace == "]":
-            closingOrder.append(2)
+            if len(openingBraces) == 0:
+                return False
+            if openingBraces[-1] != "[":
+                return False
+            openingBraces.pop()
         if brace == ")":
-            closingOrder.append(3)
-    if openingOrder[::-1] == closingOrder:
-        return True
-    print(openingOrder, closingOrder)
+            if len(openingBraces) == 0:
+                return False
+            if openingBraces[-1] != "(":
+                return False
+            openingBraces.pop()
+    if len(openingBraces) != 0:
+        return False
+    return True
 
-check_order_of_braces("(({{[[]]}}))")
+def check_number_of_braces(string):
+    openingBraces = []
+    closingBraces = []
+    for brace in string:
+        if brace == "{" or "[" or "(":
+            openingBraces.append(brace)
+        if brace == "}" or "]" or ")":
+            closingBraces.append(brace)
+    if len(closingBraces) != len(openingBraces):
+        return False
+    return True
 
-print(valid_braces("()"))
+print(valid_braces("())({}}{()][]["))
 print(valid_braces("{[}]"))
 print(valid_braces("[(])"))
 print(valid_braces("([{}])"))
