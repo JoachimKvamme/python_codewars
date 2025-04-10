35c6c1bf7384248401cde7496969a96c0d58185e
Joachim Åsen Kvamme <150397418+JoachimKvamme@users.noreply.github.com>
2025-04-10 14:36:21 +0200

    kommer nærmere på snail.py

--- snail.py
+++ snail.py
@@ -1,35 +1,40 @@
 
-array = [[1,2,3],
-         [8,9,4],
-         [7,6,5]]
+array = [[1, 2, 3, 4],
+         [12, 13, 14, 5],
+         [11, 16, 15, 6],
+         [10 ,9 ,8 ,7]]
 expected = [1,2,3,4,5,6,7,8,9]
-""" def snail(array):
-    result = []
-    for i in array[0]:
-        result.append(i)
-    result.append(array[1][-1])
-    for i in reversed(array[2]):
-        result.append(i)
-    result.append(array[1][0])
-    result.append(array[1][1])
-    return result """
 
 def snail(array):
+    if array == None or array == [[]]:
+        return []
     result = []
-    for i, subArray in enumerate(array):
-        if i == 0:
-            for x in subArray:
-                result.append(x)
-        if i == len(array) - 1:
-            for x in subArray[::-1]:
-                result.append(x)
+    top = 0
+    bottom = len(array) - 1
+    right = int(-1)
+    left = int(0)
+    for x in range(0, bottom + 1):
+        #left => right top
+        for i in range(top, bottom+1):
+            result.append(array[top][i])
+        top += 1
+        # top => bottom right
+        for i in range(top, bottom + 1):
+            result.append(array[i][right])
+        # right => left bottom
+        for i in range(top, bottom + 1):
+            reversedbottom = array[bottom][::-1]
+            result.append(reversedbottom[i])
+        bottom += -1
+        # bottom => top left
+        for i in range(top, bottom +1):
+            result.append(array[i][left])
+        left += 1
+        right -= 1
+    return result
+top = 0
+i = 0
+
+print(array[top][i])
+print(snail(array))
         
-
-
-
-    
-
-
-print(snail([[1,2,3], [8,9,4], [7,6,5]]))
-if snail(array) == expected:
-    print(True)
